6f6f0f4b0d62c35c7c0f2f3b47ea021ddda6d403
Weuller Marcos <weullermarcos@MacBook-Pro-de-Weuller.local>
2017-07-07 14:41:36 -0300

    criando filtros

--- BuscaEscolas/app/src/main/java/com/example/weullermarcos/buscaescolas/BuscaEscolasActivity.java
+++ BuscaEscolas/app/src/main/java/com/example/weullermarcos/buscaescolas/BuscaEscolasActivity.java
@@ -1,5 +1,6 @@
 package com.example.weullermarcos.buscaescolas;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -28,12 +30,18 @@ import java.util.ArrayList;
 public class BuscaEscolasActivity extends AppCompatActivity {
 
     Button btnPesquisar;
-    EditText edtNome;
+    EditText edtNome, edtMunicipio;
     TextView txtResposta;
     ListView lstEscolas;
+    Spinner spnUF;
+    Spinner spnRede;
 
     ArrayList<Escola> escolas = new ArrayList<Escola>();
     ArrayAdapter<Escola> adpEscolas;
+    ArrayAdapter<String> adpUF;
+    ArrayAdapter<String> adpRede;
+    AlertDialog alerta;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +51,26 @@ public class BuscaEscolasActivity extends AppCompatActivity {
         lstEscolas = (ListView) findViewById(R.id.busca_escolas_lstEscolas);
         txtResposta = (TextView) findViewById(R.id.busca_escolas_txtResposta);
         edtNome = (EditText) findViewById(R.id.busca_escolas_edtNome);
+        edtMunicipio = (EditText) findViewById(R.id.busca_escolas_edtMunicipio);
+        spnUF = (Spinner) findViewById(R.id.busca_escolas_spnUF);
+        spnRede = (Spinner) findViewById(R.id.busca_escolas_spnRede);
         btnPesquisar = (Button) findViewById(R.id.busca_escolas_btnPesquisar);
 
+        //configurando adapter de escolas
         adpEscolas = new ArrayAdapter<Escola>(this, android.R.layout.simple_list_item_1);
         lstEscolas.setAdapter(adpEscolas);
+
+        //configurando adapter de Rede
+        adpRede = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpRede.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnRede.setAdapter(adpRede);
+
+        //configurando adapter de UF
+        adpUF = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpUF.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnUF.setAdapter(adpUF);
+
+        populaAdapters();
 
         btnPesquisar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,9 +79,7 @@ public class BuscaEscolasActivity extends AppCompatActivity {
                 //limpa dados anteriores
                 adpEscolas.clear();
 
-                //TODO
-                //Fazer a busca de acordo com os filtros fornecidos
-                String filtro = "nome=" + edtNome.getText().toString();
+                String filtro = criarFiltro();
                 fazRequisicao(filtro);
 
             }
@@ -68,7 +90,6 @@ public class BuscaEscolasActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
                 Escola escola = (Escola) (lstEscolas.getItemAtPosition(i));
-                txtResposta.setText("Escola selecionada: " + escola.getNome());
 
                 Intent intent = new Intent(BuscaEscolasActivity.this, DetalharEscolaActivity.class);
 
@@ -84,7 +105,9 @@ public class BuscaEscolasActivity extends AppCompatActivity {
     private void fazRequisicao(String filtro){
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://mobile-aceite.tcu.gov.br:80/nossaEscolaRS/rest/escolas?"+filtro;
+        String url ="http://mobile-aceite.tcu.gov.br:80/nossaEscolaRS/rest/escolas"+filtro;
+
+        Log.d("URL", url);
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
@@ -97,6 +120,20 @@ public class BuscaEscolasActivity extends AppCompatActivity {
                 Type listType = new TypeToken<ArrayList<Escola>>(){}.getType();
                 escolas = new Gson().fromJson(response, listType);
 
+                //se não encontrou nada
+                if(escolas.size() == 0){
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(BuscaEscolasActivity.this);
+                    builder.setTitle("Aviso");
+                    builder.setMessage("Nenhuma escola encontrada para os filtros fornecidos.");
+                    builder.setNeutralButton("OK", null);
+
+                    alerta = builder.create();
+                    alerta.show();
+
+                    return;
+                }
+
                 //preenchendo adapter
                 for (Escola escola : escolas) {
 
@@ -108,8 +145,13 @@ public class BuscaEscolasActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
 
-                txtResposta.setText("Deu errado...");
+                AlertDialog.Builder builder = new AlertDialog.Builder(BuscaEscolasActivity.this);
+                builder.setTitle("Erro");
+                builder.setMessage("Erro ao buscar escolas, verifique sua conexão com a internet.");
+                builder.setNeutralButton("OK", null);
 
+                alerta = builder.create();
+                alerta.show();
             }
         });
 
@@ -117,6 +159,57 @@ public class BuscaEscolasActivity extends AppCompatActivity {
 
         //adiciona a string de requisição a fila de execução
         queue.add(stringRequest);
+
+    }
+
+    private String criarFiltro(){
+
+        String filtro = "?";
+
+        filtro += "nome=" + edtNome.getText().toString();
+        filtro += "&rede=" + spnRede.getSelectedItem().toString();
+        filtro += "&municipio=" + edtMunicipio.getText().toString();
+        filtro += "&uf=" + spnUF.getSelectedItem().toString();
+
+        Log.d("FILTRO", filtro);
+
+        return filtro;
+    }
+
+    private void populaAdapters(){
+
+        //popula spinner de rede
+        adpRede.add("Pública");
+        adpRede.add("Privada");
+
+        //popula spinner de UF
+        adpUF.add("AC");
+        adpUF.add("AL");
+        adpUF.add("AM");
+        adpUF.add("AP");
+        adpUF.add("BA");
+        adpUF.add("CE");
+        adpUF.add("DF");
+        adpUF.add("ES");
+        adpUF.add("GO");
+        adpUF.add("MA");
+        adpUF.add("MG");
+        adpUF.add("MS");
+        adpUF.add("MT");
+        adpUF.add("PA");
+        adpUF.add("PB");
+        adpUF.add("PE");
+        adpUF.add("PI");
+        adpUF.add("PR");
+        adpUF.add("RJ");
+        adpUF.add("RN");
+        adpUF.add("RO");
+        adpUF.add("RR");
+        adpUF.add("RS");
+        adpUF.add("SC");
+        adpUF.add("SE");
+        adpUF.add("SP");
+        adpUF.add("TO");
 
     }
 }
